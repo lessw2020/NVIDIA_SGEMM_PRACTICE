18e5041f3584fdde524703fd139b8767277daf06
Less Wright <lessw@etrillium.com>
2024-04-12 17:39:37 -0700

    Update kernel_1.cuh

--- src/kernel/kernel_1.cuh
+++ src/kernel/kernel_1.cuh
@@ -6,14 +6,15 @@
 #include <stdlib.h>
 
 __global__ __launch_bounds__(1024) void
-mysgemm_v1(int M, int N, int K, float alpha, float *A, float *B, float beta, float *C) {
-
-    int gx = blockIdx.x * blockDim.x + threadIdx.x; // 全局x
-    int gy = blockIdx.y * blockDim.y + threadIdx.y; // 全局y
-
+mysgemm_v1(int M, int N, int K, float alpha, float *A, float* B, float beta, float *C) {
+    int gx = blockIdx.x * blockDim.x + threadIdx.x; // global x
+    int gy = blockIdx.y * blockDim.y + threadIdx.y; // global y
+    
     float tmp = 0.;
+    
     for (int i = 0; i < K; i++) {
-        tmp += A[gy * K + i] * B[i * N + gx]; // 两次全局内存访问和一次FMA（累加乘）
+        tmp += A[gy * K + i] * B[i * N + gx]; // two global memory accesses and one FMA (fused multiply-add)
     }
+    
     C[gy * N + gx] = alpha * tmp + beta * C[gy * N + gx];
 }
